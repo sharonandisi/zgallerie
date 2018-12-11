115d24c89147194b0ca88852caca9c329c3e1d6e
sharonandisi <sharonandisi.28@gmail.com>
2018-12-11 14:16:30 +0300

    load bootstrap3 dependency to our base.html template

--- galleria/views.py
+++ galleria/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 import datetime as dt
 from django.http import HttpResponse,Http404
-from django .shortcuts import render
 
 # Create your views here.
 def welcome(request):
@@ -35,12 +34,9 @@ def archived_photos(request,past_date):
     date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
     except ValueError:
         # Raise 404 error when ValueError is thrown raise Http404()
-    day = convert_dates(date)
-    html = f'''
-    <html>
-        <body>
-            <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-        </body>
-    </html>
-            '''
-    return HttpResponse(html)
+        assert False
+    if date == dt.date.today():
+        return redirect(photos_of_the_day)
+    
+    
+    return render(reuest, 'all-photos/archived-photos.html', {"date": date})
--- zgallerie/settings.py
+++ zgallerie/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'galleria.apps.GalleriaConfig'
+    'galleria',
+    'bootstrap3',
+    'galleria.apps.GalleriaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
