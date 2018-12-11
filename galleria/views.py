90cfabcfed42363564a77722dcf106d7def79900
sharonandisi <sharonandisi.28@gmail.com>
2018-12-11 11:47:12 +0300

    getting day of the week

--- galleria/views.py
+++ galleria/views.py
@@ -11,8 +11,18 @@ def photos_of_the_day(request):
     html = f'''
         <html>
             <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1> photos for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
     return HttpResponse(html)
+def convert_dates(dates):
+
+    #Function that gets the weekday number for the date.
+
+    day_number = dt.date.weekday(dates)
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+    #Returning the actual day of the week
+    days = days[day_number]
+    return day
